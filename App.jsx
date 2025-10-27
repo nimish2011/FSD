7b231f787a9925114df40338742bdbeefc0a693c
Nimish Maheshwari <nimish2k23@gmail.com>
2025-10-27 15:06:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar.component";
+import {Routes,Route} from "react-router-dom";
+import UserAuthForm from "./page/UserAuthForm.Page";
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element = {<Navbar/>}>
+                <Route path ="signin" element={<UserAuthForm type = "sign-in"/>}/>
+                <Route path ="signup" element={<UserAuthForm type = "sign-up"/>}/>
+            </Route>
+        </Routes>
+    )
+}
+export default App;
